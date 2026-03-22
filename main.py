0f5b046a6575b2051ae45f1e65add1ebd58f8f5c
Aleksandar Sredojević <ferrumite666@gmail.com>
2026-03-22 14:14:13 +0100

    Removing unnecessary clutter, update inventory

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from product_manager import ProductManager
 
 manager = ProductManager([
     Product("Dumb phone", 350, 3),
-    Product("curved screen tv", 1500, 1)
+    Product("curved screen tv", 1500, 4)
     ])
 manager.add_product(Product("Finnie certified home fluffpile kit", 499, 20))
-
-manager.show_products()
-manager.show_total()
